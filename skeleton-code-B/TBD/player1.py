0e1919f07db1655aa210f45005dc5518d6611f45
ptrick10 <prbutcher@student.unimelb.edu.au>
2022-05-11 15:26:09 +1000

    implement capture

--- skeleton-code-B/TBD/player1.py
+++ skeleton-code-B/TBD/player1.py
@@ -1,5 +1,5 @@
 
-from referee.board import Board
+
 class Player:
     def __init__(self, player, n):
         """
@@ -14,19 +14,32 @@ class Player:
         self.self = self
         self.player = player
         self.n = n
-        self.board = {}
-        for i in range(n):
-            for j in range(n):
-                self.board[(i,j)] = 0
+        self.board = []
                 
-        self.board2 = []
-        for i in range(n):
-            lst = [0] * n
-            self.board2.append(lst)
+        for i in range(self.n):
+            lst = [0] * self.n
+            self.board.append(lst)
                 
-            
         print(self.board)
-        print(self.board2)
+        
+    def neighborhood(n, cell):
+        """
+        Function to find all the neighbours of the cell
+        """
+        neighbors = []
+        if cell[0] + 1 < n:
+            neighbors.append((cell[0]+1, cell[1]))
+            if cell[1] - 1 >= 0:
+                neighbors.append((cell[0]+1, cell[1]-1))
+        if cell[0] - 1 >= 0:
+            neighbors.append((cell[0]-1, cell[1]))
+            if cell[1] + 1 < n:
+                neighbors.append((cell[0]-1, cell[1]+1))
+        if cell[1] + 1 < n:
+            neighbors.append((cell[0], cell[1]+1))
+        if cell[1] - 1 >= 0:
+            neighbors.append((cell[0], cell[1]-1))
+        return neighbors
             
 
     def action(self):
@@ -41,7 +54,7 @@ class Player:
         # start with simple implementation
         for i in range(self.n):
             for j in range(self.n):
-                if not Board.is_occupied(self, (i, j)):
+                if not self.board[i][j]:
                     return ("PLACE", i, j)
     
     def turn(self, player, action):
@@ -55,5 +68,179 @@ class Player:
         the same as what your player returned from the action method
         above. However, the referee has validated it at this point.
         """
-        # put your code here
         
+        # put your code here
+        if player == "red":
+            self.board[action[1]][action[2]] = 1
+            
+            # check if capture has occured
+            
+            
+            r = action[1]
+            q = action[2]
+            # start with top left neighboor
+            if q > 0 and r+2 < self.n:
+                if self.board[r+1][q-1] == 2:
+                    if self.board[r+1][q] == 2:
+                        if self.board[r+2][q-1] == 1:
+                            self.board[r+1][q-1] = 0
+                            self.board[r+1][q] = 0
+            # then top right neighboor
+            if r+1 < self.n and q+1 < self.n:
+                if self.board[r+1][q] == 2:
+                    if self.board[r][q+1] == 2:
+                        if self.board[r+1][q+1] == 1:
+                            self.board[r+1][q] = 0
+                            self.board[r][q+1] = 0
+            # etc
+            if q+2 < self.n and r > 0:
+                if self.board[r][q+1] == 2:
+                    if self.board[r-1][q+1] == 2:
+                        if self.board[r-1][q+2] == 1:
+                            self.board[r][q+1] = 0
+                            self.board[r-1][q+1] = 0
+            if r-1 > 0 and q+1 < self.n:
+                if self.board[r-1][q+1] == 2:
+                    if self.board[r-1][q] == 2:
+                        if self.board[r-2][q+1] == 1:
+                            self.board[r-1][q+1] = 0
+                            self.board[r-1][q] = 0  
+            if r > 0 and q > 0 and q+1 < self.n:  
+                if self.board[r-1][q] == 2:
+                    if self.board[r][q-1] == 2:
+                        if self.board[r-1][q-1] == 1:
+                            self.board[r-1][q] = 0
+                            self.board[r][q-1] = 0
+            if q-1 > 0 and r+1 < self.n and q+1 < self.n:
+                if self.board[r][q-1] == 2:
+                    if self.board[r+1][q-1] == 2:
+                        if self.board[r+1][q-2] == 1:
+                            self.board[r][q-1] = 0
+                            self.board[r+1][q-1] = 0   
+                            
+            # now check other type of take
+            if r+1 < self.n and q > 0 and q+1 < self.n:
+                if self.board[r+1][q-1] == 2:
+                    if self.board[r+1][q] == 1:
+                        if self.board[r][q+1] == 2:
+                            self.board[r+1][q-1] = 0
+                            self.board[r][q+1] = 0
+            if r+1 < self.n and r > 0 and q+1 < self.n:
+                if self.board[r+1][q] == 2:
+                    if self.board[r][q+1] == 1:
+                        if self.board[r-1][q+1] == 2:
+                            self.board[r+1][q] = 0
+                            self.board[r-1][q+1] = 0
+            if r > 0 and q+1 < self.n:
+                if self.board[r][q+1] == 2:
+                    if self.board[r-1][q+1] == 1:
+                        if self.board[r-1][q] == 2:
+                            self.board[r][q+1] = 0
+                            self.board[r-1][q] = 0
+            if q > 0 and r > 0 and q+1 < self.n:
+                if self.board[r-1][q+1] == 2:
+                    if self.board[r-1][q] == 1:
+                        if self.board[r][q-1] == 2:
+                            self.board[r-1][q+1] = 0
+                            self.board[r][q-1] = 0
+            if r > 0 and r+1 < self.n and q > 0:
+                if self.board[r-1][q] == 2:
+                    if self.board[r][q-1] == 1:
+                        if self.board[r+1][q-1] == 2:
+                            self.board[r-1][q] = 0
+                            self.board[r+1][q-1] = 0
+            if q > 0 and r > 0 and q+1 < self.n:
+                if self.board[r][q-1] == 2:
+                    if self.board[r+1][q-1] == 1:
+                        if self.board[r+1][q] == 2:
+                            self.board[r][q-1] = 0
+                            self.board[r+1][q] = 0
+                            
+                    
+            
+            
+            
+        else:
+            self.board[action[1]][action[2]] = 2
+            
+            r = action[1]
+            q = action[2]
+            # start with top left neighboor
+            if q > 0 and r+2 < self.n:
+                if self.board[r+1][q-1] == 1:
+                    if self.board[r+1][q] == 1:
+                        if self.board[r+2][q-1] == 2:
+                            self.board[r+1][q-1] = 0
+                            self.board[r+1][q] = 0
+            # then top right neighboor
+            if r+1 < self.n and q+1 < self.n:
+                if self.board[r+1][q] == 1:
+                    if self.board[r][q+1] == 1:
+                        if self.board[r+1][q+1] == 2:
+                            self.board[r+1][q] = 0
+                            self.board[r][q+1] = 0
+            # etc
+            if q+2 < self.n and r > 0:
+                if self.board[r][q+1] == 1:
+                    if self.board[r-1][q+1] == 1:
+                        if self.board[r-1][q+2] == 2:
+                            self.board[r][q+1] = 0
+                            self.board[r-1][q+1] = 0
+            if r-1 > 0 and q+1 < self.n:
+                if self.board[r-1][q+1] == 1:
+                    if self.board[r-1][q] == 1:
+                        if self.board[r-2][q+1] == 2:
+                            self.board[r-1][q+1] = 0
+                            self.board[r-1][q] = 0   
+            if r > 0 and q > 0 and q+1 < self.n:   
+                if self.board[r-1][q] == 1:
+                    if self.board[r][q-1] == 1:
+                        if self.board[r-1][q-1] == 2:
+                            self.board[r-1][q] = 0
+                            self.board[r][q-1] = 0
+            if q-1 > 0 and r+1 < self.n and q+1 < self.n:
+                if self.board[r][q-1] == 1:
+                    if self.board[r+1][q-1] == 1:
+                        if self.board[r+1][q-2] == 2:
+                            self.board[r][q-1] = 0
+                            self.board[r+1][q-1] = 0   
+                            
+            # now check other type of take
+            if r+1 < self.n and q > 0 and q+1 < self.n:
+                if self.board[r+1][q-1] == 1:
+                    if self.board[r+1][q] == 2:
+                        if self.board[r][q+1] == 1:
+                            self.board[r+1][q-1] = 0
+                            self.board[r][q+1] = 0
+            if r+1 < self.n and r > 0 and q+1 < self.n:
+                if self.board[r+1][q] == 1:
+                    if self.board[r][q+1] == 2:
+                        if self.board[r-1][q+1] == 1:
+                            self.board[r+1][q] = 0
+                            self.board[r-1][q+1] = 0
+            if r > 0 and q+1 < self.n:
+                if self.board[r][q+1] == 1:
+                    if self.board[r-1][q+1] == 2:
+                        if self.board[r-1][q] == 1:
+                            self.board[r][q+1] = 0
+                            self.board[r-1][q] = 0
+            if q > 0 and r > 0 and q+1 < self.n:
+                if self.board[r-1][q+1] == 1:
+                    if self.board[r-1][q] == 2:
+                        if self.board[r][q-1] == 1:
+                            self.board[r-1][q+1] = 0
+                            self.board[r][q-1] = 0
+            if r > 0 and r+1 < self.n and q > 0:
+                if self.board[r-1][q] == 1:
+                    if self.board[r][q-1] == 2:
+                        if self.board[r+1][q-1] == 1:
+                            self.board[r-1][q] = 0
+                            self.board[r+1][q-1] = 0
+            if q > 0 and r > 0 and q+1 < self.n:
+                if self.board[r][q-1] == 1:
+                    if self.board[r+1][q-1] == 2:
+                        if self.board[r+1][q] == 1:
+                            self.board[r][q-1] = 0
+                            self.board[r+1][q] = 0
+            
+                    
